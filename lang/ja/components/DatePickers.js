b05ad78b71019ab984c7ca521eff9788b9479760
MEDIADO\yuka_kadokura <yuka_kadokura@mediado.jp>
2018-04-08 15:13:24 +0900

    translation DatePickers.js into ja

--- lang/ja/components/DatePickers.js
+++ lang/ja/components/DatePickers.js
@@ -1,115 +1,115 @@
 export default {
   header: 'Date/month picker',
-  headerText: 'The `v-date-picker` is stand-alone component that can be utilized in many existing Vuetify components. It offers the user a visual representation for selecting date/month.',
+  headerText: '`v-date-picker` は、多くの既存のVuetifyコンポーネントで利用できるスタンドアロンコンポーネントです。日付/月を選択するための視覚的表現をユーザに提供します。',
   components: ['v-date-picker'],
   examples: [{
     dateLight: {
-      header: 'Date pickers',
-      desc: 'Date pickers come in two orientation variations, portrait **(default)** and landscape.'
+      header: 'デートピッカーの種類',
+      desc: 'デートピッカーには、縦向き **（デフォルト）** と横向きの2つの向きのバリエーションがあります。'
     },
     dateColorable: {
-      header: 'Date pickers - Colors',
-      desc: 'Date picker colors can be set using the `color` and `header-color` props. If `header-color` prop is not provided header will use the `color` prop value.'
+      header: 'デートピッカーの色',
+      desc: 'デートピッカーの色は `color` と `header-color` を使って設定できます。 `header-color` propが与えられていなければ、ヘッダーは `color` propの値を使用します。'
     },
     dateDialogAndMenu: {
-      header: 'Date pickers - In dialog and menu',
-      desc: 'When integrating a picker into a `v-text-field`, it is recommended to use the **readonly** prop. This will prevent mobile keyboards from triggering. To save vertical space, you can also hide the picker title.  \n\n  Pickers expose a scoped slot that allow you to hook into save and cancel functionality. This will maintain an old value which can be replaced if the user cancels.'
+      header: 'ダイアログとメニューでのデートピッカー',
+      desc: 'ピッカーを `v-text-field` に統合する場合は、 `readonly` propを使用することをお勧めします。これにより、モバイルキーボードのトリガーを防ぐことができます。垂直方向のスペースを節約するために、ピッカーのタイトルを非表示にすることもできます。ピッカーはスコープされたslotをエクスポートします。このslotを使用して、保存やキャンセルをすることができます。これにより、ユーザーがキャンセルすると古い値が維持されます。'
     },
     dateAllowedDates: {
-      header: 'Date pickers - Allowed dates',
-      desc: 'You can specify allowed dates using arrays, objects, and functions.'
+      header: '選択可能な日付を指定する',
+      desc: '配列、オブジェクト、および関数を使用して、選択可能な日付を指定できます。'
     },
     dateWidth: {
-      header: 'Date pickers - Setting picker width',
-      desc: 'You can specify allowed the picker\'s width or make it full width.'
+      header: 'デートピッカーの幅の指定',
+      desc: 'デートピッカーの幅を指定したり、幅を全幅にすることができます。'
     },
     dateBirthday: {
-      header: 'Date pickers - birthday picker',
-      desc: 'Starting with year picker by default, resticting dates range and closing the picker menu after selecting the day make the perfect birthday picker.'
+      header: 'バースデーピッカーとしてのデートピッカー',
+      desc: 'デフォルトでは、年の設定から始まり、次に月を設定し、その後日を設定することで完全な誕生日を設定できます。'
     },
     datePickerDate: {
-      header: 'Date pickers - react to disaplyed month/year change',
-      desc: 'You can watch the `pickerDate` which is the displayed month/year (depending on the picker type and active view) to perform some action when it changes.'
+      header: 'デートピッカーで選択された日付の変更に応じた表示',
+      desc: 'ピッカーのタイプとアクティブなビューに応じて、選択されている日付の情報を見ることができます。変更されたときにアクションを実行します。'
     },
     dateEvents: {
-      header: 'Date pickers - Events',
-      desc: 'You can specify events using arrays, objects, and functions. To change the default color of the event use **event-color** prop.'
+      header: 'デートピッカーのイベント',
+      desc: '配列、オブジェクト、関数を使用してイベントを設定できます。イベントのデフォルトの色を変更するには、 `event-color` propを使用します。'
     },
     dateInternationalization: {
-      header: 'Date pickers - Internationalization',
-      desc: 'The date picker supports internationalization through the JavaScript Date object. Specify a BCP 47 language tag using the `locale` prop, and then set the first day of the week with the `first-day-of-week` prop.'
+      header: 'デートピッカーの国際化サポート',
+      desc: 'デートピッカーは、JavaScript Dateオブジェクトを使用して国際化をサポートしています。 `locale` propを使用してBCP 47言語タグを指定し、 `first-day-of-week` propで週の最初の日を設定します。'
     },
     dateIcons: {
-      header: 'Date pickers - icons',
-      desc: 'You can override the default icons used in the picker.'
+      header: 'デートピッカー内のアイコン',
+      desc: 'デートピッカーで使用されるデフォルトのアイコンを上書きすることができます。'
     },
     dateReadonly: {
-      header: 'Date pickers - read only',
-      desc: 'Selecting new date could be disabled by adding **readonly** prop.'
+      header: 'Read Onlyのデートピッカー',
+      desc: '`readonly` propを追加すると、新しく日付を選択することを無効にできます。'
     },
     dateCurrent: {
-      header: 'Date pickers - current date indicator',
-      desc: 'By default the current date is displayed using outline button - **show-current** prop allows you to remove the border or select different date to be displayed as the current one.'
+      header: 'デートピッカーでの現在の日付の表示',
+      desc: 'デフォルトでは、現在の日付はアウトラインボタンで表示されます。 `show-current` propを追加すると、ボーダーを削除したり、現在の日付として表示されるデフォルトの日付として異なる日付を設定できます。'
     },
     monthLight: {
-      header: 'Month pickers',
-      desc: 'Month pickers come in two orientation variations, portrait **(default)** and landscape.'
+      header: '月のデートピッカー',
+      desc: 'マンスピッカーは、縦向き **（デフォルト）** と横向きの2つの向きのバリエーションがあります。'
     },
     monthColorable: {
-      header: 'Month pickers - Colors',
-      desc: 'Month picker colors can be set using the `color` and `header-color` props. If `header-color` prop is not provided header will use the `color` prop value.'
+      header: 'マンスピッカーの色',
+      desc: 'マンスピッカーの色は、 `color` と `header-color` propを使って設定できます。 `header-color` propが与えられていなければ、ヘッダーは `color` propの値を使用します。'
     },
     monthDialogAndMenu: {
-      header: 'Month pickers - In dialog and menu',
-      desc: 'When integrating a picker into a `v-text-field`, it is recommended to use the **readonly** prop. This will prevent mobile keyboards from triggering. To save vertical space, you can also hide the picker title. \n\n Pickers expose a scoped slot that allow you to hook into save and cancel functionality. This will maintain an old value which can be replaced if the user cancels.'
+      header: 'ダイアログとメニューでのマンスピッカー',
+      desc: 'マンスピッカーを `v-text-field` に統合する場合は、 `readonly` propを使用することをお勧めします。これにより、モバイルキーボードのトリガーを防ぐことができます。垂直方向のスペースを節約するために、ピッカーのタイトルを非表示にすることもできます。ピッカーはスコープされたslotをエクスポートします。このslotを使用して、保存やキャンセルをすることができます。これにより、ユーザーがキャンセルすると古い値が維持されます。'
     },
     monthAllowedMonths: {
-      header: 'Month pickers - Allowed months',
-      desc: 'You can specify allowed months using arrays, objects, and functions.'
+      header: '選択可能な月を指定する',
+      desc: '配列、オブジェクト、および関数を使用して、選択可能な月を指定できます。'
     },
     monthWidth: {
-      header: 'Month pickers - Setting picker width',
-      desc: 'You can specify allowed the picker\'s width or make it full width.'
+      header: 'マンスピッカーの幅の指定',
+      desc: 'マンスピッカーの幅を指定したり、幅を全幅にすることができます。'
     },
     monthInternationalization: {
-      header: 'Month pickers - Internationalization',
-      desc: 'The month picker supports internationalization through the JavaScript Date object. Specify a BCP 47 language tag using the `locale` prop, and then set the first day of the week with the `first-day-of-week` prop.'
+      header: 'マンスピッカーの国際化サポート',
+      desc: 'マンスピッカーは、JavaScript Dateオブジェクトを使用して国際化をサポートしています。 `locale` propを使用してBCP 47言語タグを指定し、 `first-day-of-week` propで週の最初の日を設定します。'
     },
     monthIcons: {
-      header: 'Month pickers - icons',
-      desc: 'You can override the default icons used in the picker.'
+      header: 'マンスピッカー内のアイコン',
+      desc: 'マンスピッカーで使用されるデフォルトのアイコンを上書きすることができます。'
     },
     monthReadonly: {
-      header: 'Month pickers - read only',
-      desc: 'Selecting new date could be disabled by adding **readonly** prop.'
+      header: 'Read Onlyのマンスピッカー',
+      desc: '`readonly` propを追加すると、新しく日付を選択することを無効にできます。'
     },
     monthCurrent: {
-      header: 'Month pickers - current month indicator',
-      desc: 'By default the current month is displayed using outline button - **show-current** prop allows you to remove the border or select different month to be displayed as the current one.'
+      header: 'マンスピッカーでの現在の日付の表示',
+      desc: 'デフォルトでは、現在の日付はアウトラインボタンで表示されます。 `show-current` propを追加すると、ボーダーを削除したり、現在の月として表示されるデフォルトの月として異なる月を設定できます。'
     }
   }],
   props: {
     'v-date-picker': {
-      type: 'Determines the type of the picker - `date` for date picker, `month` for month picker',
-      monthFormat: 'Formatting function used for displaying months in the months table. Called with date (ISO 8601 string) and locale (string) arguments.',
-      allowedDates: 'Restricts which dates can be selected',
-      eventColor: 'Sets the color for event dot. It can be string (all events will have the same color) or `object` where attribute is the event date and value is the color for specified date or `function` taking date as a parameter and returning color for that date',
-      events: 'Marks the date as an event (only for date picker)',
-      locale: 'Sets the locale. Accepts a string with a BCP 47 language tag.',
-      firstDayOfWeek: 'Sets the first day of the week, starting with 0 for Sunday.',
-      titleDateFormat: 'Allows you to customize the format of the date string that appears in the title of the date picker. Called with date (ISO 8601 string) and locale (string) arguments.',
-      headerDateFormat: 'Allows you to customize the format of the month string that appears in the header of the calendar. Called with date (ISO 8601 string) and locale (string) arguments.',
-      yearFormat: 'Allows you to customize the format of the year string that appears in the header of the calendar. Called with date (ISO 8601 string) and locale (string) arguments.',
-      dayFormat: 'Allows you to customize the format of the day string that appears in the date table. Called with date (ISO 8601 string) and locale (string) arguments.',
-      yearIcon: 'Generates an icon next to the year',
-      min: 'Minimum allowed date/month',
-      max: 'Maximum allowed date/month',
-      pickerDate: 'Displayed year/month',
-      appendIcon: 'Sets the icon for next month/year button',
-      prependIcon: 'Sets the icon for previous month/year button',
-      readonly: 'Makes the picker readonly (doesnt\'t allow to select new date or navigate through months/years)',
-      scrollable: 'Allows changing displayed month with mouse scroll',
-      showCurrent: 'Toggles visibility of the current date/month outline or shows the provided date/month as a current'
+      type: 'ピッカーのタイプを決定します。デートピッカーの場合は `date` 、マンスピッカーの場合は `month` を指定します。',
+      monthFormat: '月テーブルの月の表示に使用される書式設定関数です。日付（ISO 8601文字列）とロケール（文字列）引数で呼び出されます。',
+      allowedDates: 'どの日付を選択できるかを制限するための指定をします。',
+      eventColor: 'イベントドットの色を設定します。 これは文字列（すべてのイベントは同じ色になります）または、 `object` を指定できます。attributeはイベントの日付で、valueは指定した日付の色です。または、日付をパラメータとし、その日付の色を返す `function` です。',
+      events: '日付をイベントとして印付けします（デートピッカーのみ）。',
+      locale: 'ロケールを設定します。 BCP 47言語タグを含む文字列を指定可能です。',
+      firstDayOfWeek: '日曜日を0とし、始まる週の最初の曜日を設定します。',
+      titleDateFormat: 'デートピッカーのタイトルに表示される日付の文字列の書式をカスタマイズすることができます。日付（ISO 8601文字列）と、ロケール（文字列）引数で呼び出されます。',
+      headerDateFormat: 'カレンダーのヘッダーに表示される月の文字列の書式をカスタマイズできます。日付（ISO 8601文字列）とロケール（文字列）引数で呼び出されます。',
+      yearFormat: 'カレンダーのヘッダーに表示される年の文字列の書式をカスタマイズできます。日付（ISO 8601文字列）とロケール（文字列）引数で呼び出されます。',
+      dayFormat: 'カレンダー内部に表示される日付の文字列の書式をカスタマイズできます。日付（ISO 8601文字列）とロケール（文字列）引数で呼び出されます。',
+      yearIcon: '年の隣にアイコンを配置します。',
+      min: '選択を許可する月/日の最小値を指定します。',
+      max: '選択を許可する月/日の最大値を指定します。',
+      pickerDate: '選択された年/月です。',
+      nextIcon: '次の年/月ボタンのアイコンを設定します。',
+      prevIcon: '前の年/月ボタンのアイコンを設定します。',
+      readonly: 'ピッカーを読み取り専用にします（新しい日付を選択したり、年/月を操作することはできません）。',
+      scrollable: 'マウスのスクロールで、表示月の変更ができます。',
+      showCurrent: '現在の月/日として設定された月/日の表示をするか、現在の月/日を表示するかを切り替えます。'
     }
   }
 }
